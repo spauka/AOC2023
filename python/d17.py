6db95569f0b24b313a4a5448fa531e0907769a33
Sebastian Pauka <spauka+github@gmail.com>
2023-12-22 00:27:36 +1100

    D17 optimized

--- python/d17.py
+++ python/d17.py
@@ -14,40 +14,40 @@ def dist(coord, dest):
     return np.sum(np.abs(coord - dest))
 
 def minimal_loss(g, start, dest, min_straight=1, max_straight=3):
-    to_visit = [(dist(start, dest), 0, start, ())]
+    to_visit = [(dist(start, dest), 0, start, "E", 0)]
     visited = {}
     ccoord = start
 
     with tqdm.tqdm() as pbar:
         while to_visit:
             pbar.update()
-            _, heat_loss, ccoord, last_n = heappop(to_visit)
-            if (ccoord, last_n) in visited:
-                assert visited[(ccoord, last_n)] <= heat_loss
+            _, heat_loss, ccoord, last_d, n_d = heappop(to_visit)
+            if (ccoord, last_d, n_d) in visited:
+                assert visited[(ccoord, last_d, n_d)] <= heat_loss
                 continue
-            visited[(ccoord, last_n)] = heat_loss
+            visited[(ccoord, last_d, n_d)] = heat_loss
 
             nb = g.n[ccoord]
             for d, n in zip(nb.valid_dirs, nb.coords):
                 # Check we're not going in the same direction for too long
-                if last_n and d == backwards[last_n[-1]]: # Can't reverse
+                if d == backwards[last_d]: # Can't reverse
                     continue
-                if len(last_n) == max_straight and all(ld == d for ld in last_n):
+                if n_d == max_straight and d == last_d:
                     continue
                 # Only try to turn if we can
-                if last_n and d != last_n[-1] and not all(last_n[-1] == ld for ld in last_n[-min_straight:]):
+                if n_d < min_straight and d != last_d:
                     continue
 
-                if len(last_n) == max_straight:
-                    next_path = last_n[1:] + (d,)
+                if d == last_d:
+                    nn_d = n_d + 1
                 else:
-                    next_path = last_n + (d,)
+                    nn_d = 1
                 next_cost = heat_loss+g[n]
 
-                if (n, next_path) not in visited or next_cost < visited[(n, next_path)]:
-                    heappush(to_visit, (dist(n, dest)+next_cost, next_cost, n, next_path))
+                if (n, d, nn_d) not in visited or next_cost < visited[(n, d, nn_d)]:
+                    heappush(to_visit, (dist(n, dest)+next_cost, next_cost, n, d, nn_d))
 
-    return min(loss for (ccoord, _), loss in visited.items() if ccoord == dest)
+    return min(loss for (ccoord, _, _), loss in visited.items() if ccoord == dest)
 
 g = Grid.load_dense_file("../inputs/input17", dtype=np.int16)
 start = Coord(0, g.shape[1]-1)
